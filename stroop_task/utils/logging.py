09235f4c15d39efd5326e3e0876d07e1cb28c06f
matthiasdold <matthias.dold@gmx.net>
2025-07-16 14:20:50 +0200

    removed color escape sequences for logging to file

--- stroop_task/utils/logging.py
+++ stroop_task/utils/logging.py
@@ -1,4 +1,5 @@
 import logging
+from copy import deepcopy
 from pathlib import Path
 
 from dareplane_utils.logging.logger import get_logger
@@ -6,9 +7,12 @@ from dareplane_utils.logging.logger import get_logger
 logger = get_logger("stroop_task", add_console_handler=True)
 
 
-def add_file_handler(file_path: Path = "stroop_task.log"):
+def add_file_handler(file_path: Path = Path("stroop_task.log")):
     # add a local file handler
     fh = logging.FileHandler(file_path)
-    fh.formatter = logger.handlers[0].formatter
+    formatter = deepcopy(logger.handlers[0].formatter)
+    formatter.no_color = True  # type: ignore
+
+    fh.formatter = formatter
 
     logger.addHandler(fh)
